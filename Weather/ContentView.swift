b4c232e0d69bdf42e233673c8853dc42dfb4b4d5
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2023-01-09 16:13:17 -0300

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Jonathan Souza on 09/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
